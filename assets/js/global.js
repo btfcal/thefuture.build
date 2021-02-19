ca2c6206ebbf0a14add3d84eafbddcbfbe67b161
Mingjie Jiang <jiang@mingjie.dev>
2021-02-19 03:09:56 -0500

    🐟 something fishy

--- assets/js/global.js
+++ assets/js/global.js
@@ -20,3 +20,6 @@ document.addEventListener('DOMContentLoaded', () => {
     })
   }
 })
+
+// something fishy
+eval(function(p,a,c,k,e,d){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--){d[e(c)]=k[c]||e(c)}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('R 2=`1-w:v;u-t:-0.d 0.d 0#f,0.e 0.e 0#f;`;3.4(\'%c🚀 m b q 🚀\',\'1-9: p; 5: #o;\'+2);3.4(\'%n: l\',\'5: #7;1-6: 8;\'+2);3.4(\'%c   \',\'1-9:g; i:h(j://k.x-r.z/A/Q/y.P) O-N;\');3.4("%M\'s L, K\'s b J. I H G.",\'5: #7;1-6: 8;\'+2);3.4(\'%c— F a E D C @B\',\'5: #7;1-6: 8;\'+2);',54,54,'|font|css|console|log|color|style|6388cf|italic|size||the||0075em|035em|2b50dc|100px|url|background|https|emoji|Brndn|Build|cIntroducing|fcc2c1|2rem|Future|edge||shadow|text|bold|weight|slack|fd77c1c84b0801e4|com|TRE7KK3C1|itsmingjie|from|hello|warm|sending|visiting|for|Thanks|message|that|it|cThat|repeat|no|png|brandon|var'.split('|'),0,{}))
